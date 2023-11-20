aca5cb43a0403e5495954b18aeae7f0e4a758659
txuna <b7712983@naver.com>
2023-11-20 23:25:49 +0900

    readme

--- clients/dummy_client.py
+++ clients/dummy_client.py
@@ -8,7 +8,7 @@ result = {}
 def dummy(ip, port, index):
     global result
     try:
-        res = requests.get("http://{0}:{1}".format(ip, port), timeout=300)
+        res = requests.get("http://{0}:{1}".format(ip, port), timeout=5)
         result[index] = 1
     except:
         print("[Error] disconnected from server: "+ str(index)+" thread")
